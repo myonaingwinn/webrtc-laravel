860807cecb01f73f6499c1e63271b71a3096b038
hpe <hlainepoeei@gmail.com>
2022-07-14 14:19:40 +0630

    update register form design

--- frontend/src/Login/Register.js
+++ frontend/src/Login/Register.js
@@ -4,6 +4,7 @@ import {
     Input,
 } from 'antd';
 import React, { useState } from 'react';
+import { Card } from 'antd';
 const formItemLayout = {
     labelCol: {
         xs: {
@@ -42,9 +43,7 @@ const Register = () => {
     const [form] = Form.useForm();
 
     const handleSubmit = async () => {
-        if (name === '' || email === '' || password === '') {
-            alert('field cannot be blank!');
-        } else {
+        if (!(name === '' || email === '' || password === '')) {
             await fetch("http://127.0.0.1:8000/api/v1/register", {
                 method: "POST",
                 body: JSON.stringify({
@@ -58,7 +57,7 @@ const Register = () => {
             })
                 .then((res) => {
                     console.log(res);
-                    window.location.href= '/';
+                    window.location.href = '/';
                 })
                 .catch((err) => console.log(err));
         }
@@ -78,77 +77,98 @@ const Register = () => {
     };
 
     return (
-        <Form
-            {...formItemLayout}
-            form={form}
-            name="register"
-            style=
-            {{
-                maxWidth: '700px',
-                margin: '0 auto',
-            }}
-        >
-            <Form.Item
-                name="name"
-                label="Name"
-                id="name"
-                value={name}
-                onChange={handleNameChange}
-                style={{ paddingTop: '150px' }}
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your Name!',
-                    },
-                ]}
-            >
-                <Input />
-            </Form.Item>
 
-            <Form.Item
-                name="email"
-                label="E-mail"
-                id="email"
-                value={email}
-                onChange={handleEmailChange}
-                rules={[
-                    {
-                        type: 'email',
-                        message: 'The input is not valid E-mail!',
-                    },
-                    {
-                        required: true,
-                        message: 'Please input your E-mail!',
-                    },
-                ]}
-            >
-                <Input />
-            </Form.Item>
+        <>
+            <div className="site-card-wrapper">
+                <Card title="Register Form" style=
+                    {{
+                        textAlign: 'center',
+                        width: '800px',
+                        margin: '0 auto',
+                        marginTop: '80px'
+                    }}
+                >
+                    <Form
+                        {...formItemLayout}
+                        form={form}
+                        name="register"
+                        style=
+                        {{
+                            marginTop: '30px',
+                            marginRight: '200px'
+                        }}
+                        colon={false}
+                        requiredMark={false}
+                    >
+                        <Form.Item
+                            name="name"
+                            label="Name"
+                            id="name"
+                            value={name}
+                            onChange={handleNameChange}
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your Name!',
+                                },
+                            ]}
+                        >
+                            <Input placeholder='Enter Your Name' />
+                        </Form.Item>
 
-            <Form.Item
-                name="password"
-                label="Password"
-                value={password}
-                onChange={handlePasswordChange}
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your password!',
-                    },
-                ]}
-                hasFeedback
-            >
-                <Input.Password />
-            </Form.Item>
+                        <Form.Item
+                            name="email"
+                            label="E-mail"
+                            id="email"
+                            value={email}
+                            onChange={handleEmailChange}
+                            rules={[
+                                {
+                                    type: 'email',
+                                    message: 'The input is not valid E-mail!',
+                                },
+                                {
+                                    required: true,
+                                    message: 'Please input your E-mail!',
+                                },
+                            ]}
+                        >
+                            <Input placeholder='Enter Your E-mail' />
+                        </Form.Item>
 
-            <Form.Item
-                {...tailFormItemLayout}
-            >
-                <Button type="primary" htmlType="submit" onClick={handleSubmit}>
-                    Register
-                </Button>
-            </Form.Item>
-        </Form>
+                        <Form.Item
+                            name="password"
+                            label="Password"
+                            value={password}
+                            onChange={handlePasswordChange}
+                            rules={[
+                                {
+                                    type: 'string',
+                                    min: 8,
+                                    message: 'Password must have at least 8 characters!',
+                                },
+                                {
+                                    required: true,
+                                    message: 'Please input your password!',
+                                },
+                            ]}
+                            hasFeedback
+                        >
+                            <Input.Password placeholder='Enter Your Password' />
+                        </Form.Item>
+
+                        <Form.Item
+                            {...tailFormItemLayout}
+                        >
+                            <Button type="primary" htmlType="submit" onClick={handleSubmit}>
+                                Register
+                            </Button>
+                        </Form.Item>
+
+                    </Form>
+                </Card>
+            </div>
+        </>
     );
 };
 
