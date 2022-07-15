2f3451f6671443f59edce65089e95eb6d31435d0
myonaingwinn <myonaingwin.gic@gmail.com>
2022-07-15 11:01:23 +0630

    stable state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
-import { Empty } from "antd";
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "./Dashboard/Dashboard";
+import Login from "./LoginPage/LoginPage";
+import { connectWithWebSocket } from "./utils/wssConnection/wssConnection";
 
 function App() {
+  useEffect(() => {
+    connectWithWebSocket();
+  }, []);
+
   return (
-    <div className="App">
-      <Empty />;
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
