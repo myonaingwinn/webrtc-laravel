252c595d48023fb37c6e93e411ea1104d7b816ef
hpe <hlainepoeei@gmail.com>
2022-07-18 09:01:52 +0630

    crate room page and room list page for testing

---
+++ frontend/src/Room/CreateRoom.js
@@ -0,0 +1,192 @@
+import {
+    Button,
+    Form,
+    Input,
+    Card,
+    Col,
+    Row,
+} from 'antd';
+import React, { useState, useEffect, useRef } from 'react';
+import io from "socket.io-client";
+
+const formItemLayout = {
+    labelCol: {
+        xs: {
+            span: 8,
+        },
+        sm: {
+            span: 8,
+        },
+    },
+    wrapperCol: {
+        xs: {
+            span: 24,
+        },
+        sm: {
+            span: 16,
+        },
+    },
+};
+const tailFormItemLayout = {
+    wrapperCol: {
+        xs: {
+            span: 24,
+            offset: 0,
+        },
+        sm: {
+            span: 24,
+            offset: 8,
+        },
+    },
+};
+
+const socket = io("http://localhost:5000");
+
+const CreateRoom = () => {
+    const [form] = Form.useForm();
+    const [rooms, setRooms] = useState([]);
+    const [roomName, setRoomName] = useState("");
+    const [room, setRoom] = useState("");
+    // const [participant, setParticipant] = useState("");
+    const localVideo = useRef();
+    const [display, setDisplay] = useState(true);
+    const [joinedRoom, setJoinedRoom] = useState(false);
+    const [stream, setStream] = useState();
+
+    useEffect(() => {
+        socket.on("getAllRooms", (rooms) => {
+            setRooms(rooms);
+        });
+        // Real time
+        socket.on("updateRooms", (rooms) => {
+            setRooms(rooms);
+        });
+
+        // socket.on("room_full", (room) => {
+        //     setParticipant(room.maxParticipantsAllowed);
+        //     alert("This Room has reached Maximum Number.You cannot Join!");
+        // });
+
+        if (joinedRoom === true) {
+            console.log('you are in the room!')
+        }
+    }, [rooms]);
+
+    const handleSubmit = async () => {
+        if (!(roomName === '')) {
+            setRoomName(roomName);
+            console.log('room name is ', roomName);
+            socket.emit("create_room");
+            socket.on("get_room", (room) => {
+                setRooms([...rooms, room]);
+                console.log('room id is ', room.id);
+            })
+        }
+        setDisplay(false)
+    };
+
+    const joinRoom = (room) => {
+        socket.emit("join_room", room);
+        setRoom(room.id);
+        setJoinedRoom(true);
+        console.log('you are in the ', room.id, 'room');
+        console.log('maximum participant is ', room.maxParticipantsAllowed);
+        navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
+            setStream(stream)
+            localVideo.current.srcObject = stream
+        })
+    };
+
+    return (
+        <>
+            {!joinedRoom && (
+                <div className="rooms-container" style=
+                    {{
+                        display: display ? 'none' : 'block',
+                    }}>
+                    <h2 className="rooms_heading" style={{ textAlign: 'center' }}>Available Rooms:</h2>
+
+                    {rooms.length === 0 ? (
+                        <h3 className="no_rooms">No Rooms! Create a room !</h3>
+                    ) : (
+                        <Row gutter={16}>
+                            {rooms.map((room) => {
+                                return (
+                                    <Col className="gutter-row" span={6} key={room.id}>
+                                        <Card size="small" title={room.id}>
+                                            <button onClick={() => joinRoom(room)}>Join Room</button>
+                                        </Card>
+                                    </Col>
+                                );
+                            })}
+                        </Row>
+                    )
+                    }
+                </div >
+            )}
+
+            {joinedRoom && (
+                <>
+                    <div className="video-container" style={{ marginTop: '50px' }}>
+                        <h3 style={{ textAlign: 'center' }}>Now, You are in the Room!</h3>
+                        <video playsInline muted ref={localVideo} autoPlay style={{ width: "500px", marginLeft: '500px' }} />
+                    </div>
+                </>
+            )}
+
+            <div className="site-card-wrapper" style=
+                {{
+                    display: display ? 'block' : 'none',
+                }}>
+                <Card title="Room Creation" style=
+                    {{
+                        textAlign: 'center',
+                        width: '800px',
+                        margin: '0 auto',
+                        marginTop: '80px'
+                    }}
+                >
+                    <Form
+                        {...formItemLayout}
+                        form={form}
+                        name="room"
+                        style=
+                        {{
+                            marginTop: '30px',
+                            marginRight: '200px'
+                        }}
+                        colon={false}
+                        requiredMark={false}
+                    >
+                        <Form.Item
+                            name="roomName"
+                            label="Room Name"
+                            id="name"
+                            value={roomName}
+                            onChange={(e) => setRoomName(e.target.value)}
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input room name!',
+                                },
+                            ]}
+                        >
+                            <Input placeholder='Enter room name' />
+                        </Form.Item>
+
+                        <Form.Item
+                            {...tailFormItemLayout}
+                        >
+                            <Button type="primary" htmlType="submit" onClick={handleSubmit}>
+                                Create
+                            </Button>
+                        </Form.Item>
+
+                    </Form>
+                </Card>
+            </div>
+        </>
+    );
+};
+
+export default CreateRoom;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const http = require("http");
+const { nanoid } = require("nanoid");
 const app = express();
 const server = http.createServer(app);
 require("dotenv").config();
+
 const io = require("socket.io")(server, {
   cors: {
     origin: "http://localhost:3000",
@@ -12,9 +14,47 @@ const io = require("socket.io")(server, {
 
 const PORT = process.env.SERVER_PORT || 5000;
 
+let rooms = [];
+
 app.get("/", (req, res) => {
   res.send("Server is running successfully!");
 });
+
+io.on("connection", (socket) => {
+  // Rooms
+  socket.on("create_room", () => {
+    const room = {
+      id: nanoid(7),
+      capacity: 10,
+      usersJoined: [socket.id],
+      maxParticipantsAllowed: 3,
+    };
+
+    // const myRoom = io.sockets.adapter.rooms.get(room) || { size: 0 };
+    // const numClients = myRoom.size;
+    // console.log(room, 'has', numClients, 'clients');
+    // if (numClients === room.maxParticipantsAllowed) {
+    //   socket.emit("room_full", room);
+    // }
+
+    socket.join(room);
+    socket.emit("get_room", room);
+    console.log("Room created: " + room.id);
+    rooms.push(room);
+
+    socket.broadcast.emit("updateRooms", rooms);
+  });
+
+  socket.on("join_room", (room) => {
+    socket.join(room.id);
+    console.log(`user ${socket.id} joined room: ${room.id}`);
+  });
+  socket.emit("getAllRooms", rooms);
+
+  socket.broadcast.emit("updateRooms", rooms);
+});
+
+
 
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
