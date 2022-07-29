1e1f848398408bdc1b01341032b22158d870a5d2
hpe <hlainepoeei@gmail.com>
2022-07-29 15:54:52 +0630

    modify app.js to original

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import { Empty } from "antd";
-import CreateRoom from "./Room/CreateRoom";
 
 function App() {
   return (
     <div className="App">
-      <CreateRoom />;
+      <Empty />;
     </div>
   );
 }
