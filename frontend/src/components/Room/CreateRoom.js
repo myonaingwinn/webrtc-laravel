fee7760c397b885210ebe4444c171beed01a375a
myonaingwinn <myonaingwin.gic@gmail.com>
2022-08-17 10:25:19 +0630

    add : main

--- frontend/src/components/Room/CreateRoom.js
+++ frontend/src/components/Room/CreateRoom.js
@@ -1,11 +1,24 @@
-import { Layout, Typography } from "antd";
+import { Button, Col, Layout, Typography } from "antd";
+import { v4 } from "uuid";
+import { useNavigate } from "react-router-dom";
 
 const CreateRoom = () => {
     const { Title } = Typography;
+    const navigator = useNavigate();
+
+    const createRoom = () => {
+        const id = v4();
+        navigator(`/rooms/${id}`);
+    };
 
     return (
-        <Layout className="create-room">
+        <Layout className="create-room common">
             <Title className="title">Create Room Component</Title>
+            <Col>
+                <Button type="primary" onClick={createRoom}>
+                    Create Room
+                </Button>
+            </Col>
         </Layout>
     );
 };
--- server/server.js
+++ server/server.js
@@ -2,21 +2,72 @@ const express = require("express");
 const http = require("http");
 const app = express();
 const server = http.createServer(app);
+const socket = require("socket.io");
 require("dotenv").config();
-const io = require("socket.io")(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
+const io = socket(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
 });
 
 const PORT = process.env.SERVER_PORT || 5000;
 
+const users = {};
+
+const socketToRoom = {};
+
+io.on("connection", (socket) => {
+    socket.on("join room", (roomID) => {
+        if (users[roomID]) {
+            const length = users[roomID].length;
+            if (length === 4) {
+                socket.emit("room full");
+                return;
+            }
+            users[roomID].push(socket.id);
+        } else {
+            users[roomID] = [socket.id];
+        }
+        socketToRoom[socket.id] = roomID;
+        const usersInThisRoom = users[roomID].filter((id) => id !== socket.id);
+        socket.emit("all users", usersInThisRoom);
+    });
+
+    socket.on("sending signal", (payload) => {
+        io.to(payload.userToSignal).emit("user joined", {
+            signal: payload.signal,
+            callerID: payload.callerID,
+        });
+    });
+
+    socket.on("returning signal", (payload) => {
+        io.to(payload.callerID).emit("receiving returned signal", {
+            signal: payload.signal,
+            id: socket.id,
+        });
+    });
+
+    socket.on("disconnect", () => {
+        const roomID = socketToRoom[socket.id];
+        let room = users[roomID];
+        if (room) {
+            room = room.filter((id) => id !== socket.id);
+            users[roomID] = room;
+        }
+        socket.broadcast.emit("user left", socket.id);
+    });
+
+    socket.on("change", (payload) => {
+        socket.broadcast.emit("change", payload);
+    });
+});
+
 app.get("/", (req, res) => {
-  res.send("Server is running successfully!");
+    res.send("Server is running successfully!");
 });
 
 server.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-  console.log(`http://localhost:${PORT}`);
+    console.log(`Server is listening on port ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
 });
