0efa05ac4f0e24964f4535d9c2f116c7cdadde06
myonaingwinn <myonaingwin.gic@gmail.com>
2022-08-17 10:25:53 +0630

    add : optionals

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,11 +1,15 @@
-import { Layout, Typography } from "antd";
+import { Button, Layout, Typography } from "antd";
+import { Link } from "react-router-dom";
 
 const Home = () => {
     const { Title } = Typography;
 
     return (
-        <Layout className="home">
+        <Layout className="home common">
             <Title className="title">Home Component</Title>
+            <Link to="/create_room">
+                <Button>Goto Create Room</Button>
+            </Link>
         </Layout>
     );
 };
