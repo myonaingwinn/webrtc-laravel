bdf487b78662639fe1993ae518c1b1da1b62b27f
myonaingwinn <myonaingwin.gic@gmail.com>
2022-08-29 10:43:34 +0630

    fix : UI

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import { Layout, Typography } from "antd";
+import { Layout } from "antd";
 
 import WebRTC from "./video-chat-cam.svg";
 import Video from "./video-call-svgrepo-com.svg";
@@ -8,12 +8,10 @@ import VC from "./R.svg";
 import Robot from "./robot.svg";
 
 const Home = () => {
-    const { Title } = Typography;
-
     return (
         <Layout className="home common">
             <div className="title">
-                <img src={Logo} width="130px" height="130px"></img>
+                <img src={Logo} width="130px" height="130px" alt="Logo" />
                 <span className="title" text-anchor="middle">
                     WebRTC Video Call And Message Chat{" "}
                 </span>
@@ -44,40 +42,34 @@ const Home = () => {
                     />{" "}
                 </td>
             </tr>
-            <tr className="row">
-                <td class="column-1">
-                    <img src={Chat} width="300px" height="300px"></img>
+            <tr className="middle-row">
+                <td class="column">
+                    <img src={Chat} width="300px" height="300px" alt="chat" />
                 </td>
                 <td class="column">
-                    <img src={VC} width="400px" height="400px"></img>
+                    <img src={VC} width="400px" height="400px" alt="video" />
                 </td>
                 <td class="column">
-                    <img src={Robot} width="400px" height="400px"></img>
+                    <img src={Robot} width="400px" height="400px" alt="robot" />
                 </td>
             </tr>
-            <tr className="last-row">
-                <td className="img">
-                    {" "}
-                    <img
-                        src={WebRTC}
-                        alt="React Logo"
-                        width="300px"
-                        height="300px"
-                    />{" "}
-                </td>
-
-                <td className="text1">
-                    <p className="devtext">
-                        What can WebRTC do? <br />
-                        There are many different use-cases for WebRTC, from
-                        basic web apps that uses the camera or microphone, to
-                        more advanced video-calling applications and screen
-                        sharing. We have gathered number of code samples to
-                        better illustrate how the technology works and what you
-                        can use it for.
-                    </p>
-                </td>
-            </tr>
+            <div className="last-row">
+                <img
+                    src={WebRTC}
+                    alt="React Logo"
+                    width="300px"
+                    height="300px"
+                    className="img"
+                />
+                <p className="devtext">
+                    What can WebRTC do? <br />
+                    There are many different use-cases for WebRTC, from basic
+                    web apps that uses the camera or microphone, to more
+                    advanced video-calling applications and screen sharing. We
+                    have gathered number of code samples to better illustrate
+                    how the technology works and what you can use it for.
+                </p>
+            </div>
         </Layout>
     );
 };
