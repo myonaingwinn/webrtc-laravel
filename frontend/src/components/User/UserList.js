512e9df0d46a13766be65c84a2aada7c9224ed0f
myonaingwinn <myonaingwin.gic@gmail.com>
2022-08-31 11:21:12 +0630

    update : online user list UI

--- frontend/src/components/User/UserList.js
+++ frontend/src/components/User/UserList.js
@@ -1,11 +1,14 @@
-import { Layout, Typography } from "antd";
+import { Layout, Row, Typography } from "antd";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { connectWithServer, getOnlineUsers } from "../../helpers/SocketClient";
+import { localStorageGet } from "../../helpers/Utilities";
+import User from "./User";
 
 const UserList = () => {
     const { Title } = Typography;
     const users = useSelector((state) => state.onlineUserList.value);
+    const { uuid } = localStorageGet("user") || {};
 
     useEffect(() => {
         connectWithServer();
@@ -15,13 +18,25 @@ const UserList = () => {
         document.title = "Online Users";
 
         getOnlineUsers();
-    }, []);
+    }, [users]);
 
     return (
         <Layout className="user-list common">
             <Title className="title">Online Users</Title>
-            {users && Object.keys(users).map((user) => users[user].name)}
-            <br />
+            <Row
+                gutter={[16, 32]}
+                className="user-container"
+                justify="space-evenly"
+            >
+                {users &&
+                    Object.keys(users).map(
+                        (userId) =>
+                            uuid &&
+                            uuid !== userId && (
+                                <User user={users[userId]} key={userId} />
+                            )
+                    )}
+            </Row>
         </Layout>
     );
 };
