b4586e84955af0a0326bc3d59d2a7bf10e26589c
myonaingwinn <myonaingwin.gic@gmail.com>
2022-09-01 14:41:40 +0630

    fix : setOnlineUser, HomepageUI

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import { Layout } from "antd";
+import { Col, Layout, Row } from "antd";
 
 import WebRTC from "../../assets/images/Home/video-chat-cam.svg";
 import Video from "../../assets/images/Home/video-call-svgrepo-com.svg";
@@ -7,57 +7,50 @@ import Chat from "../../assets/images/Home/chat-svgrepo.svg";
 import VC from "../../assets/images/Home/R.svg";
 import Robot from "../../assets/images/Home/robot.svg";
 import { useEffect } from "react";
+import { connectWithServer } from "../../helpers/SocketClient";
 
 const Home = () => {
     useEffect(() => {
         document.title = "Home";
-    });
+        connectWithServer();
+    }, []);
 
     return (
         <Layout className="home common">
             <div className="title">
                 <img src={Logo} width="130px" height="130px" alt="Logo" />
-                <span className="title" text-anchor="middle">
-                    WebRTC Video Call And Message Chat{" "}
+                <span className="title">
+                    WebRTC Video Call And Message Chat
                 </span>
             </div>
-            <br />
-            <tr>
-                <br />
-                <br />
-                <td className="text">
+            <Row className="first-row">
+                <Col flex={3} offset={1} className="text">
                     WebRTC, you can add real-time communication <br />
-                    capabilities to your application that works on top of
-                    <br />
-                    an open standard. It supports video, voice, and generic{" "}
-                    <br />
-                    data to be sent between peers, allowing developers to build{" "}
-                    powerful voice- and video-communication solutions.
-                    <br />
-                    <br />
-                    <br />
-                </td>
-                <td className="img1">
-                    {" "}
+                    capabilities to your application that works on top of an
+                    open standard. It supports video, voice, and generic data to
+                    be sent between peers, allowing developers to build powerful
+                    voice- and video-communication solutions.
+                </Col>
+                <Col flex={2} className="img1">
                     <img
                         src={Video}
                         alt="React Logo"
                         width="300px"
                         height="300px"
-                    />{" "}
-                </td>
-            </tr>
-            <tr className="middle-row">
-                <td class="column">
-                    <img src={Chat} width="300px" height="300px" alt="chat" />
-                </td>
-                <td class="column">
+                    />
+                </Col>
+            </Row>
+            <Row className="middle-row">
+                <Col className="column">
+                    <img src={Chat} width="400px" height="400px" alt="chat" />
+                </Col>
+                <Col className="column">
                     <img src={VC} width="400px" height="400px" alt="video" />
-                </td>
-                <td class="column">
+                </Col>
+                <Col className="column">
                     <img src={Robot} width="400px" height="400px" alt="robot" />
-                </td>
-            </tr>
+                </Col>
+            </Row>
             <div className="last-row">
                 <img
                     src={WebRTC}
