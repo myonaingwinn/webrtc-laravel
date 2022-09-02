e3ece046378fa4735a26ba499aca571271bebe44
hpe <hlainepoeei@gmail.com>
2022-09-02 09:31:33 +0630

    fix maxParticipantAllowed to original

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 
 const onlineUsers = {};
 const rooms = {};
-const maxParticipantsAllowed = 1;
+const maxParticipantsAllowed = 10;
 const socketToRoom = {};
 
 io.on("connection", (socket) => {
